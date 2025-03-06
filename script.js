20c4d23952e3a08e90e50e1e9703f4b1509fef69
Elkana Gamliel <40407541+ElkanaGam@users.noreply.github.com>
2025-03-06 08:55:14 +0200

    Update script.js skeleton code

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-﻿window.onload = function() {
-    alert("I love you");
-};
+document.addEventListener("DOMContentLoaded", () => {
+    // Make squares accept only circles
+    document.querySelectorAll(".square").forEach(square => {
+        new Sortable(square, {
+            group: "circles",
+            animation: 150,
+            onAdd: function (evt) {
+                console.log("Piece placed:", evt.item);
+            }
+        });
+    });
+
+    // Allow circles to be draggable
+    new Sortable(document.querySelector(".pieces"), {
+        group: "circles",
+        animation: 150
+    });
+});
